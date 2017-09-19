4f871a603f3f94c626a8d2b587cf9ff2573683d5
AkankshC96 <32083229+AkankshC96@users.noreply.github.com>
2017-09-19 00:05:48 -0400

    Create HuffException.java

---
+++ HuffException.java
@@ -0,0 +1,16 @@
+/**
+ *	Used in place of IOException so user does not have to use
+ *	try-catch blocks.  Used in place of RuntimeException so
+ *	that regular RuntimeExceptions are still handled in the regular
+ *	way.
+ *
+ *	@since 5 November 2016
+ */
+
+@SuppressWarnings("serial")
+public class HuffException extends RuntimeException {
+
+	public HuffException(String error) {
+		super(error);
+	}
+}
